e8cde7a34e4d4b77cd7accb91b02c2339f0af534
niv <nivlevari02@gmail.com>
2020-12-15 14:48:39 +0200

    first commit

--- src/main/java/HW2/Solution.java
+++ src/main/java/HW2/Solution.java
@@ -33,17 +33,17 @@ public class Solution {
         Connection con = DBConnector.getConnection();
         PreparedStatement pstmt = null;
         try {
-            pstmt = con.prepareStatement("INSERT INTO Test VALUES (?,?,?,?,?)");
+            pstmt = con.prepareStatement("INSERT INTO Test VALUES (?,?,?,?,?,?)");
             pstmt.setInt(1,test.getId());
             pstmt.setInt(2, test.getSemester());
             pstmt.setInt(3,test.getTime());
             pstmt.setInt(4,test.getRoom());
-            pstmt.setInt(4,test.getDay());
-            pstmt.setInt(4,test.getCreditPoints());
+            pstmt.setInt(5,test.getDay());
+            pstmt.setInt(6,test.getCreditPoints());
             pstmt.execute();
         }catch(SQLException e){
             int SQLStateNumValue = Integer.valueOf(e.getSQLState());
-            if (SQLStateNumValue== PostgreSQLErrorCodes.CHECK_VIOLATION.getValue() ||
+            if (SQLStateNumValue == PostgreSQLErrorCodes.CHECK_VIOLATION.getValue() ||
                     SQLStateNumValue == PostgreSQLErrorCodes.NOT_NULL_VIOLATION.getValue()){
                 return BAD_PARAMS;
 
@@ -76,21 +76,62 @@ public class Solution {
     public static Test getTestProfile(Integer testID, Integer semester) {
         Connection con = DBConnector.getConnection();
         PreparedStatement pstmt = null;
+        ResultSet rs = null;
+        int id = 0;
+        int sem = 0;
+        int time = 0;
+        int room = 0;
+        int day = 0;
+        int credit_points = 0;
         try {
             pstmt = con.prepareStatement("SELECT * FROM Test WHERE ID = ? and Semester = ?");
             pstmt.setInt(1, testID);
             pstmt.setInt(2, semester);
-            pstmt.execute();
+            rs =  pstmt.executeQuery();
         }catch(SQLException e){
-            int SQLStateNumValue = Integer.valueOf(e.getSQLState());
-            if (SQLStateNumValue == PostgreSQLErrorCodes.CHECK_VIOLATION.getValue()||
-                    SQLStateNumValue == PostgreSQLErrorCodes.NOT_NULL_VIOLATION.getValue()){
-                return Test.badTest();
-            }
-            else if
+            return Test.badTest();
         }
 
-        return new Test();
+        if (rs == null){   //||!rs.next()) {
+            return Test.badTest();
+        }
+
+        try {
+            id = rs.getInt(1);
+            sem = rs.getInt(2);//semester column
+            time = rs.getInt(3);
+            room = rs.getInt(4);
+            day = rs.getInt(5);
+            credit_points = rs.getInt(6);
+        }catch(SQLException e){
+            return Test.badTest();
+        }
+        finally{
+            try{
+                pstmt.close();
+            }catch(SQLException e){
+                e.printStackTrace();
+            }
+            try{
+                con.close();
+            }catch(SQLException e){
+                e.printStackTrace();
+
+            }
+        }
+
+
+        //check if it needs to be before finally
+        Test test = new Test();
+        test.setId(id);
+        test.setSemester(sem);
+        test.setTime((time));
+        test.setRoom(room);
+        test.setDay(day);
+        test.setCreditPoints(credit_points);
+        return test;
+
+
     }
 
     public static ReturnValue deleteTest(Integer testID, Integer semester) {
